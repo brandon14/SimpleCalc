22b8b83879174e662b9daa873a2ce372d99771bc
Brandon Clothier <brandon14125@gmail.com>
2013-12-12 21:51:56 -0500

    add in GNU GPL v3 notices

--- src/com/brandon14/SimpleCalc/SimpleCalcFrame.java
+++ src/com/brandon14/SimpleCalc/SimpleCalcFrame.java
@@ -1,3 +1,21 @@
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright 2013 Brandon Clothier
+ * 
+ * @author Brandon Clothier
+ */
 package com.brandon14.SimpleCalc;
 
 import java.awt.Font;
@@ -21,7 +39,7 @@ import javax.swing.UIManager;
  * This class contains all of the layouts for the calculator as well as the button Action
  * Listeners and the functions to perform the calculations and update the UI.
  * 
- * @author brandon
+ * @author Brandon Clothier
  *
  */
 public class SimpleCalcFrame extends JFrame implements ActionListener {
